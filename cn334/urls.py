1f20e60592dd5944957ac360b2ab42e5f0ea1675
KandanaiKetkaew <kandanai.ket@dome.tu.ac.th>
2024-01-28 16:02:21 +0700

    Create page views for W03-Homework

--- cn334/urls.py
+++ cn334/urls.py
@@ -6,6 +6,13 @@ from ecommerce import views as ecom_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("ecommerce/", ecom_views.ecommerce_index_view),
-    path("ecommerce/item/<item_id>", ecom_views.item_view)
-    
+    path("ecommerce/item/<item_id>", ecom_views.item_view),
+    path("homepage/", ecom_views.homepage_view),
+    path("ecommerce/category/<category_id>", ecom_views.category_view),
+    path("ecommerce/product/<product_id>", ecom_views.product_view),
+    path("ecommerce/checkout/", ecom_views.checkout_view),
+    path("ecommerce/contact/", ecom_views.contact_view),
 ]
+
+
+
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -13,3 +13,34 @@ def item_view(request, item_id):
     "item_id": item_id
 }
     return render(request, 'index.html', context = context_data)
+
+
+def homepage_view(request):
+    '''This function renders the homepage'''
+    return render(request, 'homepage.html')
+
+
+def category_view(request, category_id):
+    return render(request, 'category.html', {'category_id': category_id})
+
+
+def product_view(request, product_id):
+    
+    product = {}  
+
+    context = {
+        'product': product
+    }
+    return render(request, 'product.html', context)
+
+
+def checkout_view(request):
+    
+    order_info = {}  
+    context = {
+        'order_info': order_info
+    }
+    return render(request, 'checkout.html', context)
+
+def contact_view(request):
+    return render(request, 'contact.html')
